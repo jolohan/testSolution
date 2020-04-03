1f5505092467d533ee1e0aae04b8b911cd6eb3ff
JLOUD <JLOUD@equinor.com>
2020-04-03 18:53:26 +0200

    initial commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello world");
+            bool loopCondition = true;
+            while (loopCondition)
+            {
+                string mystring = null; 
+                Console.WriteLine("Enter your input or write 'exit' to quit: ");
+                mystring = Console.ReadLine();
+                switch (mystring?.ToLower())
+                {
+                    case "exit":
+                        Console.WriteLine("Invalid Entry.. Exiting");
+                        loopCondition = false;
+                        break;
+                    default:
+                        Console.WriteLine(mystring);
+                        break;
+                }
+            }
+        }
+    }
+
+    
+}
